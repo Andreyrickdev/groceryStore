43a31df6f6b1011c090b7e705cddb0999fb01674
Andreyrickdev <contato-andrey@outlook.com>
2022-07-16 17:19:41 -0300

    timer receives setInterval

--- assets/js/jsfile.js
+++ assets/js/jsfile.js
@@ -2,3 +2,5 @@ const myslide = document.querySelectorAll('.myslide');
 const dot = document.querySelectorAll('.dot');
 let counter = 1;
 slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
