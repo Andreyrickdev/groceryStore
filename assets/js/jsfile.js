f58cd3091a36cbb12562f7edf1d8c0deadc56d8c
Andreyrickdev <contato-andrey@outlook.com>
2022-07-16 17:30:10 -0300

    if n > myslide

--- assets/js/jsfile.js
+++ assets/js/jsfile.js
@@ -36,4 +36,9 @@ function slidefun(n) {
     for(i = 0;i<dot.length;i++) {
 		dot[i].className = dot[i].className.replace(' active', '');
 	}
+    
+    if(n > myslide.length){
+        counter = 1;
+    }
+
 }
