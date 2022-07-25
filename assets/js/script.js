b4a1d0dbe14edbbe1902fd53e8621532e95b9b6c
Andreyrickdev <contato-andrey@outlook.com>
2022-07-25 19:44:52 -0300

    added new js codes from cdn.jsdelivr

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,3 +44,29 @@ window.addEventListener('scroll', () => {
     loginForm.classList.remove('active');
     navBar.classList.remove('active');
 })
+
+/**
+ * 
+ *  Animation
+ * 
+ */
+
+ var swiper = new Swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
