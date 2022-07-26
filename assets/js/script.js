1c1191a81b2c4180851d81fc7d9fec341891ee4e
Andreyrickdev <contato-andrey@outlook.com>
2022-07-26 13:30:09 -0300

    other animation js

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,28 @@ window.addEventListener('scroll', () => {
       },
     },
   });
+
+    /**
+   * other animation
+   */
+
+     var swiper = new Swiper(".review-slider", {
+      loop: true,
+      spaceBetween: 20,
+      autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+      },
+      centeredSlides: true,
+      breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1020: {
+          slidesPerView: 3,
+        },
+      },
+    });
